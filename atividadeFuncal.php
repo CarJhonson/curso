75ab5ee5c9360e1499f16f48c3d6c2ba67b0444e
Aluno <aluno@edu.senac.br>
2018-11-20 22:19:44 -0200

    codigos das atividades1

--- atividadeFuncal.php
+++ atividadeFuncal.php
@@ -1,20 +1,42 @@
 <?php
 
- function operacao($tipo,$num1,$num2){
- 	switch($tipo){
- 		case 'soma':
- 		return $num1+$num2;
- 		break;
- 		case 'sub':
- 		return $num1-$num2;
- 		break;
- 		case 'mult':
- 		return $num1*$num2;
- 		break;
- 		case 'div':
- 		return $num1/$num2;
- 		break;
- 	}
+function calculadora($tipo,$num1,$num2){
+	switch($tipo){
+		case 'soma':
+		echo "A $tipo entre $num1 e $num2 é igual a  ".($num1+$num2);
+		break;
+		case 'subtração':
+		echo "A $tipo entre $num1 e $num2 é igual a  ".($num1-$num2);
+		break;
+		case 'multiplicação':
+		echo "A $tipo entre $num1 e $num2 é igual a  ".($num1*$num2);
+		break;
+		case 'divisão':
+		echo "A $tipo entre $num1 e $num2 é igual a  ".($num1/$num2);
+		break;
+		default:
+		echo 'Tipo passado não é válido! informe: soma, subtração, multiplicação, divisão';
+	}
 }
- echo operacao("div",5,5);
+echo calculadora("subtraçã",5,5);
+
+function calculadoraIf($tipo, $num1, $num2){
+	if($tipo=='soma'){
+		$resultado = "A $tipo entre $num1 e $num2 é igual a <strong>".($num1 + $num2)."</strong>";
+	}else if($tipo=='subtração'){
+		$resultado = "A $tipo entre $num1 e $num2 é igual a <strong>".($num1 - $num2)."</strong>";
+	}else if($tipo=='multiplicação'){
+		$resultado = "A $tipo entre $num1 e $num2 é igual a <strong>".($num1 * $num2)."</strong>";
+	}else if($tipo=='divisão'){
+		$resultado = "A $tipo entre $num1 e $num2 é igual a <strong>".($num1 / $num2)."</strong>";
+	}else{
+		$resultado='Tipo passado não é válido! informe: soma, subtração, multiplicação, divisão';
+	}
+	return $resultado;
+}
+
+echo '<br>';
+echo '<br>';
+echo calculadoraIf("diviso",6,6);
+
 ?>
